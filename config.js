4d2a945b7ba122164e21784cddc28c2eb0e106f2
GOURAV PUNANI <gouravpunani222@gmail.com>
2017-08-11 10:35:54 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports={
+  consumer_key:         'IifbRiHk79YJ4mpY69fpyxv8Y',
+  consumer_secret:      '5nWbJBuPxint1Keg78S36VvtzP9ibpYnYgLFfYbad34Gftb3ZX',
+  access_token:         '2428390003-VhwriOPaJb87Kno5Z8pdZ6LllAgnl2pOyM1htT4',
+  access_token_secret:  'aZcn0EC2GEQ1cwnuyysRAKXnHsheSu0W2PQgEnjrkLQB8',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+};
